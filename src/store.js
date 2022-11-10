930d7b94d8c8d636e6734fd8a12551edf455483f
Hannah.Celemen <hcelemen@icloud.com>
2022-11-10 00:45:55 -0500

    Add changes to counter arrangement

--- src/store.js
+++ src/store.js
@@ -1,4 +1,7 @@
+// named exports inside the curly braces DO need to match the function/variable from the file you are exporting
 import { configureStore } from '@reduxjs/toolkit';
+
+// name of the default import/export doesnt matter, this is equivalent to the counterSlice.reducer
 import counterReducer from './counter/counterSlice';
 
 export default configureStore({
